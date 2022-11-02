c76fb3fb8d0c91d8d274032c6538a1c01791dc07
CHOIWONSEOK <chws900@gmail.com>
2022-11-02 17:49:26 +0900

    feature: now can cweet cweet

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,35 @@
-import React from "react";
-const Home =() => <span>Home</span>
+import { addDoc, collection } from "firebase/firestore";
+import { fireStore } from "myBase";
+import React, {useState} from "react";
+const Home =() => {
+    const [cweet, setCweet] = useState("");
+    const onSubmit = async (event) => {
+        console.log(event)
+        event.preventDefault();
+        const docRef = await addDoc(
+        collection(fireStore,"cweets"),
+        {
+            cweet,
+            createdAt: Date.now()
+        });
+        setCweet("");
+    }
+    const onChange = (event) => {
+        console.log(event)
+        const {
+            target: {
+                value
+            }
+        } = event;
+        setCweet(value)
+    }
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input value={cweet} type="text" placeholder="What's on your mind?" maxLength={120} onChange={onChange}/>
+                <input type="submit" value="cwit"  />
+            </form>
+        </div>
+    )
+}
 export default Home;
