f822e8cf454371a771efd8f609d2781c65f5ddde
CHOIWONSEOK <chws900@gmail.com>
2022-11-03 17:24:12 +0900

    feature: upload file

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,10 +2,13 @@ import { addDoc, collection, doc, getDoc, getDocs, onSnapshot, orderBy, query } 
 import { fireStore } from "myBase";
 import React, {useEffect, useState} from "react";
 import Cweet from '../components/Cweet'
+import { getStorage, ref, uploadString } from "firebase/storage";
+import { v4 } from "uuid";
 const Home =({ userObj }) => {
     console.log(userObj)
     const [cweet, setCweet] = useState("");
     const [cweets, setCweets] = useState([]);
+    const [attachment, setAttachment] = useState();
     const getCweet = async () => {
         const cweetData = await getDocs(query(collection(fireStore, "cweets")))
         cweetData.forEach((document) => {
@@ -29,16 +32,23 @@ const Home =({ userObj }) => {
         
     }, [])
     const onSubmit = async (event) => {
-        console.log(event)
         event.preventDefault();
-        const docRef = await addDoc(
-        collection(fireStore,"cweets"),
+        // const docRef = await addDoc(
+        /* collection(fireStore,"cweets"),
         {
             text: cweet,
             createdAt: Date.now(),
             creatorId: userObj.uid,
         });
-        setCweet("");
+        setCweet(""); */
+        const storage = getStorage();
+        const imageRef = ref(storage, `${userObj.uid}/${v4()}`);
+
+        uploadString(imageRef, attachment, "data_url").then((snapshot) => {
+            console.log("UPloaded base64 encoded string")
+        })
+        
+
     }
     const onChange = (event) => {
         console.log(event)
@@ -49,13 +59,36 @@ const Home =({ userObj }) => {
         } = event;
         setCweet(value)
     }
+    const onFileChange = (event) => {
+        const {target: {files},} = event;
+        const theFile = files[0]
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            const {
+                currentTarget: {
+                    result
+                }
+            } =finishedEvent;
+            setAttachment(result);
+        }
+        reader.readAsDataURL(theFile);
+    }
+    const onClearAttachment = () => setAttachment(null);
     console.log("cweets")
     console.log("cwee"+cweets)
     return (
         <div className="container">
             <form onSubmit={onSubmit}>
                 <input value={cweet} type="text" placeholder="What's on your mind?" maxLength={120} onChange={onChange}/>
+                <input type="file" accept="image/*" onChange={onFileChange} />
                 <input type="submit" value="cwit"  />
+                {attachment && 
+                (
+                <div>
+                    <img src={attachment} width="50px" height="50px" />
+                    <button onClick={onClearAttachment}>Clear</button>
+                </div>)}
+
             </form>
             <div>
                 { cweets.map((cweetObj) => (
